7b170d6c69bd39b0c909f88440d97e00d8df671a
Nothando <nk.mashabane@gmail.com>
2023-01-10 11:27:23 -0800

    fixes

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -6,5 +6,5 @@ import json
 
 
 def from_json_string(my_str):
-        """Returns the Python object representation of a JSON string"""
-            return json.loads(my_str)
+    """Returns the Python object representation of a JSON string"""
+    return json.loads(my_str)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/pyhton3
-"""This module adds all arguments to a Python list and save them to a file."""
+import json
+"""
+This file contains a function that
+writes an obj to a text file using
+JSON rep
+"""
 
 
-import sys
-
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    """
+    function that writes an obj  to a text file
+    """
+    with open(filename, mode="w") as myFile:
+        json.dump(my_obj, myFile)
