1f7fece4f4c30b629d071f90f467de5427f8f7a3
Nothando <nk.mashabane@gmail.com>
2023-01-10 11:48:47 -0800

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     """
     function that writes an obj  to a text file
     """
-    with open(filename, "w", encoding="UTF-8") as f:
-            json.dump(my_obj, f)
+    with open(filename, mode="w") as f:
+            f.write(json.dumps(my_obj))
