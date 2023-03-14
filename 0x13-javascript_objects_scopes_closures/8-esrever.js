a1f3caf8f5e474772f9691cfb58a3bb2cd843b21
nothando <nk.mashabane@gmail.com>
2023-03-14 05:39:33 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
+
 exports.esrever = function (list) {
-  let reverse = [];
-  let i = list.length - 1;
-  while (i >= 0) {
-    reverse.push(list[i]);
-    i--;
-  }
-  return reverse;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
