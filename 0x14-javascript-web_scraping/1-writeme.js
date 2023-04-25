6a7a9f801780c34b736da1708d8c67e4624ccff0
Nothando Mashabane <nk.mashabane@gmail.com>
2023-04-25 18:17:58 +0000

    fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-// Script that writes a string to a file
-let fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
-  if (err) {
-    return console.log(err);
-  }
+const fs = require('fs');
+fs.appendFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) throw err;
 });
