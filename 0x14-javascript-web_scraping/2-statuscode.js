e8734b910a2db8fb26b4d8d6c4ecaafe03f4b6c6
Nothando Mashabane <nk.mashabane@gmail.com>
2023-04-25 18:14:26 +0000

    fixx

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-// Script that displays the status code of a GET request
-let request = require('request');
-request.get(process.argv[2], function (err, response) {
-  if (err) {
-    return console.log(err);
-  } else {
-    console.log('code: ' + response.statusCode);
-  }
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log('code: ' + response.statusCode);
 });
